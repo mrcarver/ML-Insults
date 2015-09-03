0d4b6f9c52caa2e61a74ab87e341e5ac149a38c1
CrBillman <Ateowa@gmail.com>
2015-09-03 13:05:03 -0400

    X Fixed an error that lead to incorrect classifications of insults, which raised the efficiency on the training set to 90+%. X Added documentation to Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,40 @@
-## Insult Detection Kaggle Competition!
+###################################################################################################################
+##                                                                                                               ##
+##  Main function for classifying comments as insulting or non-insultin to users within an internet forum        ##
+##  setting.  This code is meant to solve the Insult Detection Kaggle competition, which can be found at         ##
+##  https://www.kaggle.com/c/detecting-insults-in-social-commentary.  Currently, this code contains the          ##
+##  following methods for categorization:                                                                        ##
+##           X Naive Bayes Rule                                                                                  ##
+##                                                                                                               ##
+###################################################################################################################
+
+# Fcts is the local module for functions used for insult detection
 import Fcts
+# Numpy is an external module (That may require loading on HPC/NERSC) that allows C-like matrix manipulation and math.
 import numpy as np
+
+# Initalizes a dictionary, which will be filled based solely on the components of the training set.
 
 Dictionary=[]
 
+# Calls a function to read the data from the training set.  This function is specifically tailored for the format of train.csv.  It returns a numpy matrix
+# That is formatted as :
+#	[0] : flag for insult, 1 for insult, 0 for non-insult
+#	[1] : Year comment was posted
+#	[2] : Month comment was posted
+#	[3] : Day comment was posted
+#	[4] : Hour comment was posted
+#	[5] : Minute comment was posted
+#	[6] : Second comment was posted
+#	[7] : Comment
+# If the timestamp is not included, elements 1-6 are filled with "N/A".
+
 Train_Set=Fcts.Read_Data("train.csv")
 N_Train=len(Train_Set)
-Word_Count=[]
-Insult_Count=0
+
+# Creates the dictionary, and counts the number of insults for use in later statistics.  All dictionary elements are lower case, and do not have apostrophes, quotations.
+
+N_Insult=0
 for i in xrange(0,N_Train):
 	Words=Train_Set[i][7].split()
 	for j in xrange(0,len(Words)):
@@ -18,14 +45,24 @@ for i in xrange(0,N_Train):
 		if(Word_Check not in Dictionary):
 			Dictionary.append(Word_Check)
 	if(Train_Set[i][0]==1):
-		Insult_Count=Insult_Count+1
+		N_Insult=N_Insult+1
 
-Word_Count=np.zeros([len(Train_Set),len(Dictionary)])
-NB_Prob=Fcts.Naive_Bayes(Train_Set,Dictionary,Insult_Count)
+print N_Insult, N_Train
+
+# Calls a function to generate the probability needed for using the Naive Bayes Rule for insult classification.  NB_Prob is a 3 x (N_words) matrix, which contains three probabilities for each word:
+#	[0] : Probability this word is encountered in a comment in the training set
+#	[1] : Probability this word is encountered in an insult in the training set
+#	[2] : Probability this word is encountered in a non-insult in the training set
+
+NB_Prob=Fcts.Naive_Bayes(Train_Set,Dictionary,N_Insult)
+
+# The NB_Prob matrix is used to generate probabilities for insult classification.  The Bayes rule is more rigorously calculated by summing ln(1-p)-ln(p) for the probability p of each word.  Then,
+# this summ is put into a logistic function, 1/(1+e^(Prob_Sum)).  This loop also grades the algorithm by returning the probability we get a correct
+
 Num_Right=0
 False_Positive=0
+Correct_Insults=0
 for i in xrange(0,N_Train):
-#for i in xrange(0,1):
 	Words=Train_Set[i][7].split()
 	Prob_Sum=0.0
 	for j in xrange(0,len(Words)):
@@ -33,20 +70,23 @@ for i in xrange(0,N_Train):
 		Word_Check=Word_Check.strip('.')
 		Word_Check=Word_Check.lower()
 		Word_Index=Dictionary.index(Word_Check)
-		Ind_Prob=NB_Prob[1,Word_Index]*(Insult_Count/float(N_Train))/(NB_Prob[1,Word_Index]*(Insult_Count/float(N_Train))+NB_Prob[2,Word_Index]*(1-Insult_Count/float(N_Train)))
+		Ind_Prob=NB_Prob[1,Word_Index]*(N_Insult/float(N_Train))/(NB_Prob[1,Word_Index]*(N_Insult/float(N_Train))+NB_Prob[2,Word_Index]*(1-N_Insult/float(N_Train)))
 		Prob_Sum=Prob_Sum+(np.log(1-Ind_Prob)-np.log(Ind_Prob))
 	if(Prob_Sum>40.0):
 		Insult_Prob=0.0
-	elif(np.isinf(Prob_Sum)):
-		Insult_Prob=0.0
 	elif(np.isneginf(Prob_Sum)):
 		Insult_Prob=1.0
+	elif(np.isinf(Prob_Sum)):
+		Insult_Prob=0.0
 	else:
 		Insult_Prob=1/(1+np.exp(Prob_Sum))
-	if(round(Train_Set[i][0])==Insult_Prob):
+	if(Train_Set[i][0]==round(Insult_Prob)):
 		Num_Right=Num_Right+1
-	if((Train_Set[i][0]>=0.5) and (Insult_Prob==0)):
+	if((Train_Set[i][0]==0) and (Insult_Prob>=0.5)):
 		False_Positive=False_Positive+1
+	if((Train_Set[i][0]==1) and (Insult_Prob>=0.5)):
+		Correct_Insults=Correct_Insults+1
 
-print Num_Right/float(N_Train)
-print False_Positive/float(N_Train)
+print "Probability of getting a correct classification: " + str(Num_Right/float(N_Train))
+print "Probability of generating a false positive: " + str(False_Positive/float(N_Train-N_Insult))
+print "Probability of correctly identifying insults: " + str(Correct_Insults/float(N_Insult))
